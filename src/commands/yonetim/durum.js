d0fdf0a1c141326010a2b71c8e21f7f13e08a9f1
Eren Ertugrul <erenertugrul@gmail.com>
2019-09-28 22:12:21 +0300

    hmm

--- src/commands/yonetim/durum.js
+++ src/commands/yonetim/durum.js
@@ -33,7 +33,7 @@ class durum extends Command
     }
     
     hasPermission(msg) {
-        if(!msg.guild) return this.client.isOwner(msg.author);
+        //if(!msg.guild) return this.client.isOwner(msg.author);
         if ((msg.channel.type !== "dm")) return false;
         return msg.member.hasPermission('Admin') || this.client.isOwner(msg.author) || (msg.member.id == "174242106766786570");
     }
