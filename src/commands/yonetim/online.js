22c51ba860aa3848f74f8e08fd3a1b6545ad81d2
Eren Ertugrul <erenertugrul@gmail.com>
2019-09-29 19:26:12 +0300

    bot durumu

---
+++ src/commands/yonetim/online.js
@@ -0,0 +1,42 @@
+const { Command } = require('discord.js-commando');
+class online extends Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'online',
+            group: 'yonetim',
+            memberName: 'online',
+            description: 'bot online durmunu değiştir ',
+            examples: ['online'],
+            args: [
+                {
+                  key: 'online',
+                  prompt: 'Online durumu seçin? \n online \n idle \n invisible \n dnd',
+                  type: 'string',
+                }
+            ]
+        })
+    }
+    /*
+    hasPermission(msg) {
+        if(!msg.guild) return this.client.isOwner(msg.author);
+        if ((msg.channel.type !== "dm")) return false;
+        return msg.member.hasPermission('Admin') || this.client.isOwner(msg.author) || (msg.member.id == "174242106766786570");
+    }
+  */
+    run(msg,online) {
+      let arg = JSON.stringify(online);
+      let j = JSON.parse(arg);
+      if (msg.channel.type == "dm")
+      {
+        if ((msg.author.id == "174242106766786570") || (msg.author.id == "478933409276624896"))
+        {
+          this.client.user.setStatus(j.online)
+        }
+      }
+    }
+}
+
+
+module.exports = online;
